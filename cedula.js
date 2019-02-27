aa5d2cbcc3877f854e02e8357c769e09f67fdf4b
Demostenes Garcia <d.garcia@ifarhu.gob.pa>
2019-02-27 08:26:19 -0500

    Adding support for 6 digits on 'tomo' section of the cedula, for cases like E-8-102017, as previously found by IFARHU

--- cedula.js
+++ cedula.js
@@ -20,7 +20,6 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 */
 
-
 /**
  * Validates a Panamenial id (Cédula)
  *
@@ -28,11 +27,14 @@ SOFTWARE.
  *
  */
 function validateCedula(cedula) {
-  Array.prototype.insert = function (index, item) {
+  Array.prototype.insert = function(index, item) {
     this.splice(index, 0, item);
   };
-  var re=/^P$|^(?:PE|E|N|[23456789]|[23456789](?:A|P)?|1[0123]?|1[0123]?(?:A|P)?)$|^(?:PE|E|N|[23456789]|[23456789](?:AV|PI)?|1[0123]?|1[0123]?(?:AV|PI)?)-?$|^(?:PE|E|N|[23456789](?:AV|PI)?|1[0123]?(?:AV|PI)?)-(?:\d{1,4})-?$|^(PE|E|N|[23456789](?:AV|PI)?|1[0123]?(?:AV|PI)?)-(\d{1,4})-(\d{1,5})$/i
-  var matched = cedula.match(re)
+
+  var re = /^P$|^(?:PE|E|N|[23456789]|[23456789](?:A|P)?|1[0123]?|1[0123]?(?:A|P)?)$|^(?:PE|E|N|[23456789]|[23456789](?:AV|PI)?|1[0123]?|1[0123]?(?:AV|PI)?)-?$|^(?:PE|E|N|[23456789](?:AV|PI)?|1[0123]?(?:AV|PI)?)-(?:\d{1,4})-?$|^(PE|E|N|[23456789](?:AV|PI)?|1[0123]?(?:AV|PI)?)-(\d{1,4})-(\d{1,6})$/i;
+
+  var matched = cedula.match(re);
+
   // matched contains:
   // 1) if the cedula is complete (cedula = 8-AV-123-123)
   //    matched = [cedula, first part, second part, third part]
@@ -40,32 +42,41 @@ function validateCedula(cedula) {
   // 2) if the cedula is not complete (cedula = "1-1234")
   //    matched = ['1-1234', undefined, undefined, undefined]
   var isComplete = false;
+
   if (matched !== null) {
-    matched.splice(0,1); // remove the first match, it contains the input string.
-    if (matched[0] !== undefined) { // if matched[0] is set => cedula complete
+    matched.splice(0, 1); // remove the first match, it contains the input string.
+
+    if (matched[0] !== undefined) {
+      // if matched[0] is set => cedula complete
       isComplete = true;
+
       if (matched[0].match(/^PE|E|N$/)) {
-        matched.insert(0,'0');
+        matched.insert(0, "0");
       }
+
       if (matched[0].match(/^(1[0123]?|[23456789])?$/)) {
-        matched.insert(1,'');
+        matched.insert(1, "");
       }
+
       if (matched[0].match(/^(1[0123]?|[23456789])(AV|PI)$/)) {
         var tmp = matched[0].match(/(\d+)(\w+)/);
-        matched.splice(0,1);
-        matched.insert(0,tmp[1]);
-        matched.insert(1,tmp[2]);
+
+        matched.splice(0, 1);
+        matched.insert(0, tmp[1]);
+        matched.insert(1, tmp[2]);
       }
     } // matched[0]
   }
 
   var result = {
-      'isValid': cedula.length==0? true : re.test(cedula),
-      'inputString': cedula,
-      'isComplete': isComplete,
-      'cedula': isComplete ? matched.splice(0,4) : null
+    isValid: cedula.length === 0 ? true : re.test(cedula),
+    inputString: cedula,
+    isComplete: isComplete,
+    cedula: isComplete ? matched.splice(0, 4) : null
   };
-//  console.log(result);
+
+  //  console.log(result);
+
   return result;
 }
 
